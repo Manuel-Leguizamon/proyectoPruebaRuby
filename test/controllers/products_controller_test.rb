cac19b00ecf30573e31fa2e964d95d27f0c3560a
manuel <estebanmanuelleguizamon@gmail.com>
2024-09-03 08:26:05 -0400

    se agrega la logica del test 'render a detailed product page'

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -7,7 +7,12 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
         assert_select '.product',2
     end
     test 'render a detailed product page' do
+        get product_path(products(:ps4))
 
+        assert_response :success
+        assert_select '.title','PS4 Fat'
+        assert_select '.description','PS4 en buen estado'
+        assert_select '.price','150'
     end
 
 end
