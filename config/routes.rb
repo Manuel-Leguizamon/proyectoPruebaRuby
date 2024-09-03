e3ec6b4d390bba31ade7f13c628f151e2bb96fea
manuel <estebanmanuelleguizamon@gmail.com>
2024-09-03 12:45:48 -0400

    Se agrega la funcion create para cargar la base de datos con un producto ingresado

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  post '/products', to: 'products#create'
   get '/products/new', to: 'products#new', as: :new_product
-  get '/products', to: 'products#index'
+  get '/products', to: 'products#index' 
   get '/products/:id', to: 'products#show', as: :product
 
 end
