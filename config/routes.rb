70ba8505288c0f995f050d7ffc01465c3fcc5f01
manuel <estebanmanuelleguizamon@gmail.com>
2024-09-04 08:41:01 -0400

    Se agrega la ruta 'poducts/:id/edit'

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   get '/products/new', to: 'products#new', as: :new_product
   get '/products', to: 'products#index' 
   get '/products/:id', to: 'products#show', as: :product
+  get '/products/:id/edit', to: 'products#edit', as: :product_edit
 
 end
