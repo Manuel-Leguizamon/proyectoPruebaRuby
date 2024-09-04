41a06dd6c2179cad32d14234d6180b0847e269e2
manuel <estebanmanuelleguizamon@gmail.com>
2024-09-04 11:13:35 -0400

    Se agrega el metodo update para productos

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  patch '/products/:id', to: 'products#update'
   post '/products', to: 'products#create'
   get '/products/new', to: 'products#new', as: :new_product
   get '/products', to: 'products#index' 
   get '/products/:id', to: 'products#show', as: :product
-  get '/products/:id/edit', to: 'products#edit', as: :product_edit
+  get '/products/:id/edit', to: 'products#edit', as: :edit_product
 
 end
